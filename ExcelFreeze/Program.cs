71c5b69cb052eb3d28ea7fb7457ba4fc87128985
Tufan DAYI <tdayi@hotmail.com>
2017-12-11 16:13:27 +0300

    Excel Freeze Sample
    Microsoft Office Interop Excel Freeze Sample Project

---
+++ ExcelFreeze/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ExcelFreeze
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Excel = Microsoft.Office.Interop.Excel;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel.Application application = new Excel.Application();
+            Excel.Workbook workbook = application.Workbooks.Open(@"C:\test.xlsx");
+            Excel.Worksheet worksheet = workbook.Sheets[1];
+
+            worksheet.Activate();
+            worksheet.Application.ActiveWindow.SplitRow = 1;
+            worksheet.Application.ActiveWindow.FreezePanes = true;
+
+            Excel.Range firstRow = (Excel.Range)worksheet.Rows[1];
+            firstRow.AutoFilter(1,
+                                Type.Missing,
+                                Excel.XlAutoFilterOperator.xlAnd,
+                                Type.Missing,
+                                true);
+
+            workbook.Save();
+            workbook.Close(Type.Missing, Type.Missing, Type.Missing);
+            application.Quit();
+            Marshal.FinalReleaseComObject(application);
+        }
+    }
+}
